82d76cf26b7e8310e1dc9ca1e21ae9cffbf4ff79
Kay <dustocyno@gmail.com>
2018-01-14 18:17:45 -0800

    MNIST CNN in tensorflow/pytorch

---
+++ 20171114-mnist-torch-linear.py
@@ -0,0 +1,166 @@
+# ==============================================================================
+# Torch MNIST CNN classifier + without activation (linear NN)
+# --------------------------------------------------------------------------
+# Copied from https://github.com/pytorch/examples/blob/master/mnist/main.py
+#
+# To test effect of nonlinearity
+# ==============================================================================
+
+from __future__ import print_function
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+
+def get_parser():
+    import argparse
+    parser = argparse.ArgumentParser(description='PyTorch MNIST Example')
+    parser.add_argument('--batch-size', type=int, default=256, metavar='N',
+                        help='input batch size for training (default: 256)')
+    parser.add_argument('--test-batch-size', type=int, default=1000, metavar='N',
+                        help='input batch size for testing (default: 1000)')
+    parser.add_argument('--epochs', type=int, default=10, metavar='N',
+                        help='number of epochs to train (default: 10)')
+    parser.add_argument('--lr', type=float, default=0.01, metavar='LR',
+                        help='learning rate (default: 0.01)')
+    parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
+                        help='SGD momentum (default: 0.5)')
+    parser.add_argument('--cuda', action='store_true', default=False,
+                        help='enables CUDA training')
+    parser.add_argument('--seed', type=int, default=1, metavar='S',
+                        help='random seed (default: 1)')
+    parser.add_argument('--log-interval', type=int, default=15, metavar='N',
+                        help='how many batches to wait before logging training status')
+    return parser
+
+
+def load_mnist_data(save_dir, kwargs):
+    train_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(save_dir, train=True, download=True,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081, ))
+            ])),
+        batch_size=args.batch_size, shuffle=True, **kwargs)
+    test_loader = torch.utils.data.DataLoader(
+        datasets.MNIST(save_dir, train=False, 
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize((0.1307,), (0.3081, ))
+            ])),
+        batch_size=args.test_batch_size, shuffle=True, **kwargs)
+    return (train_loader, test_loader)
+
+
+class Net(nn.Module):
+
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_channels=1,
+            out_channels=10,
+            kernel_size=5,
+            stride=1,
+            padding=0)
+        self.conv2 = nn.Conv2d(
+            in_channels=10,
+            out_channels=20,
+            kernel_size=5)
+        self.conv2_drop = nn.Dropout2d()
+        self.fc1 = nn.Linear(320, 50)
+        self.fc2 = nn.Linear(50, 10)
+
+    def forward(self, x):
+        '''
+        # Input x shape (1, 28^2)
+        x = F.relu(F.max_pool2d(self.conv1(x), 2)) # 10, 12^2
+        x = F.relu(F.max_pool2d(self.conv2_drop(self.conv2(x)), 2)) # 20, 4^2
+        x = x.view(-1, 320) # Flatten
+        x = F.relu(self.fc1(x))
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+        '''
+        return self.forward_lin(x)
+
+    def forward_lin(self, x):
+        x = F.max_pool2d(self.conv1(x), 2) # 10, 12^2
+        x = F.max_pool2d(self.conv2_drop(self.conv2(x)), 2) # 20, 4^2
+        x = x.view(-1, 320) # Flatten
+        x = self.fc1(x)
+        x = F.dropout(x, training=self.training)
+        x = self.fc2(x)
+        return F.log_softmax(x)
+
+
+def main(args):
+    
+    # ============
+    # Set up Model
+    # ------------
+    kwargs = {'num_workers':1, 'pin_memory':True} if args.cuda else {}
+    train_loader, test_loader = load_mnist_data('../data', kwargs)
+
+    model = Net()
+    if args.cuda:
+        model.cuda()
+
+    optimizer = optim.SGD(
+        model.parameters(), 
+        lr=args.lr, 
+        momentum=args.momentum)
+
+    metrics = {"loss":[]}
+
+    # ============
+    # Train and Test
+    # ------------
+    def train(epoch):
+        model.train()
+        for batch_idx, (data, target) in enumerate(train_loader):
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data), Variable(target)
+            optimizer.zero_grad()
+            output = model(data)
+            loss = F.nll_loss(output, target)
+            loss.backward()
+            optimizer.step()
+            metrics["loss"].append(loss.data[0])
+            if batch_idx % args.log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                    epoch, batch_idx * len(data), len(train_loader.dataset),
+                    100. * batch_idx / len(train_loader), loss.data[0]))
+
+    def test():
+        model.eval()
+        test_loss = 0
+        correct = 0
+        for data, target in test_loader:
+            if args.cuda:
+                data, target = data.cuda(), target.cuda()
+            data, target = Variable(data, volatile=True), Variable(target)
+            output = model(data)
+            test_loss += F.nll_loss(output, target, size_average=False).data[0] # sum up batch loss
+            pred = output.data.max(1, keepdim=True)[1] # get the index of the max log-probability
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+
+        test_loss /= len(test_loader.dataset)
+        print('\nTest set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
+            test_loss, correct, len(test_loader.dataset),
+            100. * correct / len(test_loader.dataset)))
+    
+    for epoch in range(args.epochs + 1):
+        train(epoch)
+        test()
+
+if __name__ == '__main__':
+    args = get_parser().parse_args()
+    args.cuda = args.cuda and torch.cuda.is_available()
+    torch.manual_seed(args.seed)
+    if args.cuda:
+        torch.cuda.manual_seed(args.seed)
+    main(args)
